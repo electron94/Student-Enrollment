8b0aeb00a1c1aaf5b67c721cb9192bb12f4e545e
Lordjesussaves94 <pallavi.gandham@tekreant.com>
2023-12-29 16:31:52 +0530

    dffg

--- client/app/Users/page.tsx
+++ client/app/Users/page.tsx
@@ -1,9 +1,12 @@
 'use client'
 
 import React, { useState } from 'react';
+import {useForm} from 'react-hook-form'
 
 
 const EnrollmentForm: React.FC = () => {
+    const[isModified,setIsModified]=useState<undefined | string>(undefined);
+    const [selectedCourse, setSelectedCourse] = useState('');
   const [studentData, setStudentData] = useState({
     Rollno:'',
     name: '',
@@ -11,6 +14,12 @@ const EnrollmentForm: React.FC = () => {
     course: '',
     phone:'',
   });
+
+  const handleCourseChange = (e:any) => {
+    setSelectedCourse(e.target.value);
+  };
+
+  const{register,trigger,formState:{errors},setValue}=useForm();
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setStudentData({
@@ -26,12 +35,13 @@ const EnrollmentForm: React.FC = () => {
   };
 
   return (
-    <div className='row mt-5'>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <div className='w-50 border bg-light p-5'>
       <h1><b>Student Enrollment Form</b></h1>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit} className='bg-white rounded p-4 w-120 shadow-md'>
       <div className='form-group mb-3 p-2'>
           <label htmlFor="Rollno" className='text-sm font-medium text-grey-700'>Rollno:</label>
-          <input className='w-full border rounded p-2'
+          <input className='w-full border rounded p-2' 
             type="number"
             id="Rollno"
             name="Rollno"
@@ -43,52 +53,77 @@ const EnrollmentForm: React.FC = () => {
 
         <div className='form-group mb-3 p-2'>
           <label htmlFor="name"  className='text-sm font-medium text-grey-700'>Name:</label>
-          <input className='w-full border rounded p-2'
-            type="text"
-            id="name"
-            name="name"
-            value={studentData.name}
-            onChange={handleChange}
-            required
-          />
+          <input className={`w-full border rounded p-2 ${errors.name && errors.name.type === "required" ? "border-red-500" : "border-gray-300"}`} type='text' id='name'
+            {...register("name", {required: true, minLength: 4}  ) }
+             onBlur={() => trigger("name")}
+             onChange={(e) =>{
+                const trimmedValue=e.target.value.replace(/[^A-Za-z]/gi,'');
+                setStudentData({name:trimmedValue});
+              setValue("name", trimmedValue);
+              trigger("name");
+              setIsModified(true);
+            }}
+             />
+             {errors.name && errors.name.type === "required" &&  <p className='text-red-500 text-sm'>Please enter the name</p>}
+            {errors.name && errors.name.type === "minLength" && <p className='text-yellow-500 text-sm'>Please enter at least 4 characters</p>}
+           
         </div>
         <div className='form-group mb-3 p-2'>
           <label htmlFor="email"  className='text-sm font-medium text-grey-700'>Email:</label>
           <input className='w-full border rounded p-2'
+             {...register("email", { required: true,pattern: {
+                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i}}) }
+                onBlur={() => trigger("email")}
             type="email"
             id="email"
             name="email"
             value={studentData.email}
-            onChange={handleChange}
-            required
+            onChange={(e) =>{
+                setStudentData(prevData => ({ ...prevData, email: e.target.value }));
+            setValue("email",e.target.value);
+            trigger('email');
+            setIsModified(true);
+            }}
           />
+          {errors.email && errors.email && errors.email.type==="required" && <p className='text-red-500 text-sm'>please enter the email</p>}
+              {errors.email && errors.email.type==="pattern" && <p className='text-yellow-500 text-sm'>please enter valid email</p>}
         </div>
-        <div className='form-group mb-3 p-2'>
-          <label htmlFor="course"  className='text-sm font-medium text-grey-700'>Course:</label>
-          <input className='w-full border rounded p-2'
-            type="text"
-            id="course"
-            name="course"
-            value={studentData.course}
-            onChange={handleChange}
-            required
-          />
-        </div>
+        <div>          
+            <label htmlFor="course"> Course:</label>         
+             <select id="course" name="course" value={selectedCourse} onChange={handleCourseChange}>    
+             <option value="">Select..</option>     
+                  <option value="sql">Sql</option>      
+                        <option value="python">python</option>        
+                            <option value="c">c</option>     
+                                   <option value="java">java</option>  
+
+            </select>    
+       </div> 
         <div className='form-group mb-3 p-2'>
           <label htmlFor="phone"  className='text-sm font-medium text-grey-700'>phone:</label>
           <input className='w-full border rounded p-2'
+            {...register("phone", { required: true,pattern:{value:/^[0-9]{10}$/ }}) }
             type="number"
             id="phone"
             name="phone"
+            onBlur={() => trigger("phone")}
             value={studentData.phone}
-            onChange={handleChange}
-            required
+            onChange={(e) =>{
+             
+                setStudentData(prevData => ({ ...prevData, phone: e.target.value }));
+               setValue("phone",e.target.value);
+                trigger('phone');
+                setIsModified(true);          
+               }}
           />
+             {errors.phone&&errors.phone && errors.phone.type==="required"&&<p className='text-red-500 text-sm'>please enter number</p>}
+              {errors.phone && errors.phone.type==="pattern"&&<p className='text-green-500 text-sm'>please enter exactly 10 digits and only enter digits</p>}
         </div>
-        <button type="submit">Submit</button>
-        <button type="submit">Login</button>
+        <button className="bg-blue-500 hover:bg-blue-600 rounded-lg px-4" type="submit"> submit</button>
+        <button className="bg-blue-500 hover:bg-blue-600 rounded-lg px-4" type="submit"> Login</button>
       </form>
     </div>
+    </main>
   );
 };
 
--- client/app/page.tsx
+++ client/app/page.tsx
@@ -9,14 +9,21 @@ import Router from 'next/router';
 const HomePage: React.FC = () => {
   return (
     <div>
-      <h1>Welcome to the Main Page</h1>
-      <Link href="/Users">
+      <h1><b>Welcome to the Main Page</b></h1>
+    
         
-          <button>User</button>
-        
+       <div >
+         <button className="bg-blue-500 hover:bg-blue-600 rounded-lg px-4 py-2 mb-2" style={{ marginLeft: '40px' }} >User Login</button>
+         <Link href="/Users">
+         <button className="bg-blue-500 hover:bg-blue-600 rounded-lg px-4 py-2" style={{ marginLeft: '40px' }} >User SignUp</button>
+         </Link>
+       </div>
+    
+      <Link href="/Admin">
+        <div>
+      <button className="bg-blue-500 hover:bg-blue-600 rounded-lg px-2 py-2" style={{ marginLeft: '1500px' }} >Admin Login</button>
+      </div>
       </Link>
-      <button>Admin</button>
-
     </div>
   );
 };
