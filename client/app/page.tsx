4839cff7db7ae3d045046cea86c50867c56de987
Lordjesussaves94 <pallavi.gandham@tekreant.com>
2023-12-29 13:43:33 +0530

    edgfg

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,14 +1,20 @@
-
+'use client'
 // Example in your main page
 import React from 'react';
 import Link from 'next/link';
+import Router from 'next/router';
+
 
 
 const HomePage: React.FC = () => {
   return (
     <div>
       <h1>Welcome to the Main Page</h1>
-      <button>User</button>
+      <Link href="/Users">
+        
+          <button>User</button>
+        
+      </Link>
       <button>Admin</button>
 
     </div>
