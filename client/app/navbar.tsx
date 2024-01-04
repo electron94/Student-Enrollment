c2e863258e56c05c14598f2d50d9aac1d4c6b2f1
Lordjesussaves94 <pallavi.gandham@tekreant.com>
2024-01-04 15:01:10 +0530

    dfh

--- client/app/navbar.tsx
+++ client/app/navbar.tsx
@@ -1,9 +1,22 @@
+import { ShieldCheckIcon, UserIcon } from "@heroicons/react/24/solid";
+import Link from "next/link";
+import React from "react";
+ 
+ 
 export default function Navbar() {
-    return(
-        <div>
-            <h1 className= "bg-blue-500 text-white p-4">Student Enrollment</h1>
-        </div>
-    )
-    
-
+  return (
+    <nav className="flex justify-between items-center bg-blue-500 px-6 py-3">
+      <h1 className="text-white font-bold">Student Enrollment</h1>
+      <div>
+        <Link className="text-white font-bold" href={"/Users/Login"}>
+          <UserIcon className="h-5 w-5 mr-1" />
+          Users
+        </Link>
+        <Link className="text-white font-bold" href={"/Admin/Login"}>
+          <ShieldCheckIcon className="h-5 w-5 mr-1" />
+          Admin
+        </Link>
+      </div>
+    </nav>
+  );
 }
