7464881d8836396d515fe52e3d4d9214dcaa5be0
Lordjesussaves94 <pallavi.gandham@tekreant.com>
2024-01-04 11:50:31 +0530

    users

--- client/app/Users/View/page.tsx
+++ client/app/Users/View/page.tsx
@@ -1,153 +1,157 @@
 'use client'
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react'
+import { useParams, useRouter } from 'next/navigation';
 import axios from 'axios';
-import {useForm} from  'react-hook-form';
-import { useRouter } from 'next/navigation';
  
  
-const View = () => {
-  const { register, formState: {errors}, trigger } = useForm();
+const view = () => {
  
-  interface UserData {
-    Rollno: string;
-    name: string;
-    phone: string;
-    email: string;
-    course:string;
-    password:string
-  }
-  const [selectedCourse, setSelectedCourse] = useState('');
-  const [studentData,setstudentData]=useState('');
-    const [data, setData] = useState<UserData>({
-      Rollno: '',
-      name: '',
-      phone: '',
-      email: '',
-      course:'',
-      password:''
-    });
  
-    const handleCourseChange = (e:any) => {
+  const [student, setStudent] = useState<any>({
+  });
+  const router = useRouter();
+  const{id} = useParams();
  
-        setSelectedCourse(e.target.value);
-      };
  
   useEffect(() => {
-    let studentemail:any=localStorage.getItem("studentemail")
-    console.log(studentemail)
+    let userId:any=localStorage.getItem("UserId")
+    console.log(userId)
   }, []);
+ 
+ 
   useEffect(() => {
     const fetchData = async () => {
-      let studentemail:any=localStorage.getItem("studentemail")
       try {
-     
+        let userId = localStorage.getItem("UserId");
+        const body = {
+          studentID: userId
+        }
+       
+        const response = await axios.post<any>('http://localhost:3004/student/show', body);
+       
+        const studentData = (response.data)
+        setStudent(studentData.response);
  
-  const response = await axios.get(`http://localhost:3000/Users`);
-          setData(response.data);
-          console.log("hksdkgkds",response.data.phone)
-      } catch (error) {
-        console.error(error);
+        console.log('Student Data:', studentData);
+      } catch (err) {
+        console.error('Error fetching student data:', err);
       }
     };
  
     fetchData();
   }, []);
  
-   
-  const router = useRouter();
  
-     
+ 
+ 
+useEffect(() => {
+let studentid:any=localStorage.getItem("studentID")
+console.log("jgkkjj",studentid
+)
+}, []);
+ 
+const handleUpdate = (e: { preventDefault: () => void; }) => {
+e.preventDefault();
+console.log('Updating with student data:', student);
+ 
+ 
+let userId = localStorage.getItem("UserId");
+if (!userId) {
+    console.error('User ID not found in localStorage.');
+    return;
+  }
+ 
+const body = {
+    StudentID: userId,
+    Rollno: student.rollno,
+    name: student.name,
+    email: student.email,
+    course: student.course,
+    phone: student.phone,
+    password: student.password
+  }
+ 
+console.log('Update request body:', body);
+axios.post('http://localhost:3004/student/update', body)
+  .then(result => {
+    console.log('Update result:', result);
+    router.push('/');
+  })
+  .catch(err => console.error('Error updating student:', err));
+}
+ 
   return (
-  <main className="flex min-h -screen flex-col items-center justify-between p-24">
-      <div className='w-90 h-90 mx-auto shadow p-5'>
-        <h2 className='text-center mb-4'>View UserDetails</h2>
-        <form className='bg-white rounded p-4 w-120 shadow-md'>
-        <div className='form-group mb-3 p-2'>
-          <label htmlFor="Rollno" className='text-sm font-medium text-grey-700'>Rollno:</label>
-          <input className='w-full border rounded p-2'
-            type="number"
-            id="Rollno"
-            name="Rollno"
-            value={studentData.Rollno}
-            required
-          />
-        </div>
-          <div className='form-group mb-3 p-2'>
-          <label htmlFor="name"  className='text-sm font-medium text-grey-700'>Name:</label>
-          <input className={`w-full border rounded p-2 ${errors.name && errors.name.type === "required" ? "border-red-500" : "border-gray-300"}`} type='text' id='name'
-            {...register("name", {required: true, minLength: 4}  ) }
-             onBlur={() => trigger("name")}
-             value={studentData.name}
-             
-           
-             />
-             {errors.name && errors.name.type === "required" &&  <p className='text-red-500 text-sm'>Please enter the name</p>}
-            {errors.name && errors.name.type === "minLength" && <p className='text-yellow-500 text-sm'>Please enter at least 4 characters</p>}
-           
-        </div>
-         
-          <div className='form-group mb-3 p-2'>
-          <label htmlFor="email"  className='text-sm font-medium text-grey-700'>Email:</label>
-          <input className='w-full border rounded p-2'
-             {...register("email", { required: true,pattern: {
-                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i}}) }
-                onBlur={() => trigger("email")}
-            type="email"
-            id="email"
-            name="email"
-            value={studentData.email}
-           
-          />
-          {errors.email && errors.email && errors.email.type==="required" && <p className='text-red-500 text-sm'>please enter the email</p>}
-              {errors.email && errors.email.type==="pattern" && <p className='text-yellow-500 text-sm'>please enter valid email</p>}
-        </div>
-          <div>          
-            <label htmlFor="course"> Course:</label>        
-             <select id="course" name="course" value={selectedCourse} onChange={handleCourseChange}>    
-             <option value="">Select..</option>    
-                  <option value="sql">Sql</option>      
-                        <option value="python">python</option>        
-                            <option value="c">c</option>    
-                                   <option value="java">java</option>  
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <div className='d-flex w-100 vh-100 justify-contect-center align-items-center'>
+    <div className='w-50 border bg-light p-5'>
+     <h2 className='text-center mb-4'>View User</h2>
+     <form onSubmit={handleUpdate} className='bg-white rounded p-4 w-120 shadow-md'>    
  
-            </select>    
-       </div>
-       <div className='form-group mb-3 p-2'>
-          <label htmlFor="phone"  className='text-sm font-medium text-grey-700'>phone:</label>
-          <input className='w-full border rounded p-2'
-            {...register("phone", { required: true,pattern:{value:/^[0-9]{10}$/ }}) }
-            type="number"
-            id="phone"
-            name="phone"
-            onBlur={() => trigger("phone")}
-            value={studentData.phone}
-         
-          />
-             {errors.phone&&errors.phone && errors.phone.type==="required"&&<p className='text-red-500 text-sm'>please enter number</p>}
-              {errors.phone && errors.phone.type==="pattern"&&<p className='text-green-500 text-sm'>please enter exactly 10 digits and only enter digits</p>}
+     <div className='form-group mb-3 p-2'>
+           <label htmlFor='Rollno' className="text-sm font-medium text-gray-700">Rollno:</label>
+           <input className={'w-full border rounded p-2 $ {errors.rollno && errors.rollno.type==="required" ? "border-red-500" : "border-gray-300"}'}
+            type='text'
+            placeholder='enter the Rollno'
+            value={student.rollno}
+            onChange={(e) => setStudent({ ...student, rollno:e.target.value} )}
+            />
         </div>
         <div className='form-group mb-3 p-2'>
-          <label htmlFor="password"  className='text-sm font-medium text-grey-700'>password:</label>
-          <input className='w-full border rounded p-2'
-            {...register("password", { required: true}) }
-            type="text"
-            id="password"
-            name="password"
-            onBlur={() => trigger("password")}
-            value={studentData.password}
-         
-          />
-             {errors.password&&errors.password && errors.password.type==="required"&&<p className='text-red-500 text-sm'>please enter password</p>}
-             
+           <label htmlFor='name' className="text-sm font-medium text-gray-700">Name:</label>
+           <input className={'w-full border rounded p-2 $ {errors.name && errors.name.type==="required" ? "border-red-500" : "border-gray-300"}'}
+            type='text'
+            placeholder='enter the name'
+            value={student.name}
+            onChange={(e) => setStudent({ ...student, name:e.target.value} )}
+            />
         </div>
-        
+        <div className='form-group mb-3 p-2'>
+            <label htmlFor='email'  className="text-sm font-medium text-gray-700">Email:</label>
+            <input className={'w-full border rounded p-2 $ {errors.name && errors.name.type==="required" ? "border-red-500" : "border-gray-300"}'}
+             type='email'
+             placeholder='enter the email'
+             value={student.email}
+             onChange={(e) => setStudent({ ...student, email: e.target.value })}
+           
+            />
+        </div>
+        <div className='form-group mb-3 p-2'>
+            <label htmlFor='course'  className="text-sm font-medium text-gray-700">course:</label>
+            <input className={'w-full border rounded p-2 $ {errors.name && errors.name.type==="required" ? "border-red-500" : "border-gray-300"}'}
+             type='text'
+             placeholder='select the course'
+             value={student.course}
+             onChange={(e) => setStudent({ ...student, course: e.target.value })}
+           
+            />
+        </div>
+       
+        <div className='form-group mb-3 p-2'>
+            <label htmlFor='phone' className="text-sm font-medium text-gray-700">Phone:</label>
+            <input className={'w-full border rounded p-2 $ {errors.phone && errors.phone.type==="required" ? "border-red-500" : "border-gray-300"}'}
+             type='number'
+             placeholder='enter the phone'
+             value={student.phone}
+             onChange={(e) => setStudent({ ...student, age: e.target.value })}
+            />
+        </div>
  
+        <div className='form-group mb-3 p-2'>
+            <label htmlFor='password'  className="text-sm font-medium text-gray-700">password:</label>
+            <input className={'w-full border rounded p-2 $ {errors.name && errors.name.type==="required" ? "border-red-500" : "border-gray-300"}'}
+             type='text'
+             placeholder='enter the password'
+             value={student.password}
+             onChange={(e) => setStudent({ ...student, password: e.target.value })}
+           
+            />
+        </div>
+        <button type="submit" className="bg-blue-500 hover:bg-blue-600 rounded-lg px-6 py-3">Update</button>
+     </form>
+    </div>
+   </div>
+</main>
+  )
+}
  
-        </form>
-      </div>
-  </main>
-  );
-};
- 
-export default View;
- 
+export default view
