d3c883e9b0772164098ff1afd72f02b523bc9dce
Lordjesussaves94 <pallavi.gandham@tekreant.com>
2024-01-09 10:55:20 +0530

    fg

--- client/app/Users/View/page.tsx
+++ client/app/Users/View/page.tsx
@@ -57,53 +57,13 @@ const view = () => {
   
     fetchData();
   }, []);
-  
-  
- 
- 
- 
- 
-useEffect(() => {
-let studentid:any=localStorage.getItem("studentID")
-console.log("jgkkjj",studentid
-)
-}, []);
- 
-const handleUpdate = (e: { preventDefault: () => void; }) => {
-e.preventDefault();
-console.log('Updating with student data:', student);
- 
- 
-let userId = localStorage.getItem("UserId");
-if (!userId) {
-    console.error('User ID not found in localStorage.');
-    return;
-  }
- 
-const body = {
-    StudentID: userId,
-    name: student.name,
-    email: student.email,
-    course: student.course,
-    phone: student.phone,
-    password: student.password
-  }
- 
-console.log('Update request body:', body);
-axios.post('http://localhost:3004/student/update', body)
-  .then(result => {
-    console.log('Update result:', result);
-    router.push('/');
-  })
-  .catch(err => console.error('Error updating student:', err));
-}
  
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
     <div className='d-flex w-100 vh-100 justify-contect-center align-items-center'>
     <div className='w-50 border bg-light p-5'>
      <h2 className='text-center mb-4'>View User</h2>
-     <form onSubmit={handleUpdate} className='bg-white rounded p-4 w-120 shadow-md'>    
+     <form  className='bg-white rounded p-4 w-120 shadow-md'>    
  
    
         <div className='form-group mb-3 p-2'>
@@ -112,7 +72,7 @@ axios.post('http://localhost:3004/student/update', body)
             type='text'
             placeholder='enter the name'
             value={student?.name}
-            onChange={(e) => setStudent({ ...student, name:e.target.value} )}
+           
             />
         </div>
         <div className='form-group mb-3 p-2'>
@@ -121,7 +81,7 @@ axios.post('http://localhost:3004/student/update', body)
              type='email'
              placeholder='enter the email'
              value={student?.email}
-             onChange={(e) => setStudent({ ...student, email: e.target.value })}
+            
            
             />
         </div>
@@ -131,7 +91,7 @@ axios.post('http://localhost:3004/student/update', body)
              type='text'
              placeholder='select the course'
              value={student?.course}
-             onChange={(e) => setStudent({ ...student, course: e.target.value })}
+           
            
             />
         </div>
@@ -142,7 +102,7 @@ axios.post('http://localhost:3004/student/update', body)
              type='number'
              placeholder='enter the phone'
              value={student?.phone}
-             onChange={(e) => setStudent({ ...student, age: e.target.value })}
+            
             />
         </div>
  
@@ -152,7 +112,7 @@ axios.post('http://localhost:3004/student/update', body)
              type='text'
              placeholder='enter the password'
              value={student?.password}
-             onChange={(e) => setStudent({ ...student, password: e.target.value })}
+            
            
             />
         </div>
